22f15999b9fded4d0c0cb76681dd3e5bb2c56bbf
lgdelacruz <lgdelacruz@lgdelacruz.com>
2019-12-02 12:23:52 -0800

    Complete

---
+++ rollup.config.js
@@ -0,0 +1,25 @@
+import babel from "rollup-plugin-babel";
+
+export default [
+  {
+    input: "src/ToolsPanel/ToolsPanel.js",
+    output: {
+      name: "bt-tools-panel",
+      file: "dist/index.js",
+      format: "es"
+    },
+    external: [
+      "react",
+      "@material-ui/core",
+      "clsx",
+      "@material-ui/icons",
+      "react-draggable",
+      "prop-types"
+    ],
+    plugins: [
+      babel({
+        exclude: "node_modules/.**"
+      })
+    ]
+  }
+];
