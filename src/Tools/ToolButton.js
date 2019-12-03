7d9bcd5bb159dc79ad8166720a25804cda5b637a
lgdelacruz <lgdelacruz@lgdelacruz.com>
2019-12-02 17:36:54 -0800

    Tools button click

--- src/Tools/ToolButton.js
+++ src/Tools/ToolButton.js
@@ -1,8 +1,23 @@
 import React from "react";
 import PropTypes from "prop-types";
+import * as MUI from "@material-ui/core";
+import ToolsPanel from "./ToolsPanel";
 
 const ToolButton = props => {
-  return <div></div>;
+  const { icon, id } = props;
+  const [open, setOpen] = React.useState(false);
+  return (
+    <React.Fragment>
+      <MUI.Fab onClick={() => setOpen(true)} id={id}>
+        {icon}
+      </MUI.Fab>
+      <ToolsPanel
+        id={`${id}-panel`}
+        onClose={() => setOpen(false)}
+        open={open}
+      />
+    </React.Fragment>
+  );
 };
 
 ToolButton.propTypes = {
--- src/__tests__/ToolButton.test.js
+++ src/__tests__/ToolButton.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import ToolButton from "../Tools/ToolButton";
 import * as MUIIcon from "@material-ui/icons";
@@ -7,6 +7,23 @@ import * as MUIIcon from "@material-ui/icons";
 describe("Test Tools Panel", () => {
   test("render tool button", () => {
     renderDefault({ icon: <MUIIcon.AcUnit /> });
+  });
+
+  test("contains icon", () => {
+    const toolButton = renderDefault({
+      icon: <MUIIcon.AcUnit data-testid="icon" />
+    });
+    expect(toolButton.getByTestId("icon")).toBeVisible();
+  });
+
+  test("contains icon", () => {
+    const toolButtonId = "tool-button";
+    const toolButton = renderDefault({
+      id: toolButtonId,
+      icon: <MUIIcon.AcUnit data-testid="icon" />
+    });
+    fireEvent.click(toolButton.getByTestId("icon"));
+    expect(document.getElementById(`${toolButtonId}-panel`)).toBeVisible();
   });
 
   const renderDefault = props => {
