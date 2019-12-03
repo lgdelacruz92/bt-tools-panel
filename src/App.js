8efa25fae1475160a98662577f1e1002a5da926e
lgdelacruz <lgdelacruz@lgdelacruz.com>
2019-12-02 17:14:54 -0800

    Clean state

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import React from "react";
 import "./App.css";
-import BtToolsPanel from "bttoolspanel-bloom";
+import ToolsPanel from "./ToolsPanel/ToolsPanel";
+import * as MUI from "@material-ui/core";
+import * as MUIIcons from "@material-ui/icons";
+
+const useStyles = MUI.makeStyles(theme => {
+  return {
+    fab: {
+      position: "absolute",
+      top: 100,
+      left: 100
+    }
+  };
+});
 
 function App() {
   return <div className="App"></div>;
