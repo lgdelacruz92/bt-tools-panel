7671401a1e75b7be853f881a5171e134ac5592a4
lgdelacruz <lgdelacruz@lgdelacruz.com>
2019-12-04 16:11:37 -0800

    Final version

--- src/App.js
+++ src/App.js
@@ -26,26 +26,31 @@ function App() {
   const classes = useStyles();
   return (
     <div className="App">
-      <div style={{ width: 100, height: 3000, background: "lightcoral" }} />
-      <ToolButton
-        className={classes.toolButton1}
-        icon={<MUIIcon.AcUnit />}
-        panelPosition="left"
-      >
-        <div style={{ width: 100, height: 100, background: "red" }}></div>
-        <div style={{ width: 200, height: 100, background: "blue" }}></div>
-        <div style={{ width: 100, height: 200, background: "green" }}></div>
-      </ToolButton>
+      <div
+        style={{ width: 100, height: 100, background: "blue", flex: 1 }}
+      ></div>
+      <div style={{ background: "lightblue", flex: 4 }}>
+        <div style={{ width: 100, height: 3000, background: "lightcoral" }} />
+        <ToolButton
+          className={classes.toolButton1}
+          icon={<MUIIcon.AcUnit />}
+          panelPosition="left"
+        >
+          <div style={{ width: 100, height: 100, background: "red" }}></div>
+          <div style={{ width: 200, height: 100, background: "blue" }}></div>
+          <div style={{ width: 100, height: 200, background: "green" }}></div>
+        </ToolButton>
 
-      <ToolButton
-        className={classes.toolButton2}
-        icon={<MUIIcon.AcUnit />}
-        panelPosition="right"
-      >
-        <div style={{ width: 100, height: 100, background: "red" }}></div>
-        <div style={{ width: 200, height: 100, background: "blue" }}></div>
-        <div style={{ width: 100, height: 200, background: "green" }}></div>
-      </ToolButton>
+        <ToolButton
+          className={classes.toolButton2}
+          icon={<MUIIcon.AcUnit />}
+          panelPosition="right"
+        >
+          <div style={{ width: 100, height: 100, background: "red" }}></div>
+          <div style={{ width: 200, height: 100, background: "blue" }}></div>
+          <div style={{ width: 100, height: 200, background: "green" }}></div>
+        </ToolButton>
+      </div>
     </div>
   );
 }
